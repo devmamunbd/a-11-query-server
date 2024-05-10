45f66e936005017ba2c1e21183c1f19de9520bbd
dymamunbd <mdmamunfedev@gmail.com>
2024-05-10 22:43:49 +0600

    delete done

--- index.js
+++ index.js
@@ -54,7 +54,13 @@ async function run() {
       res.send(result)
     })
 
-
+    //delete 
+    app.get('/delete/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await AssElevenCollenction.deleteOne(query)
+      res.send(result)
+    })
 
 
 
