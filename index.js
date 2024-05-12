a1042d35663ec60885b74d3d5cf232a23fed36f8
dymamunbd <mdmamunfedev@gmail.com>
2024-05-12 13:28:03 +0600

    my reco delete button done

--- index.js
+++ index.js
@@ -128,6 +128,13 @@ async function run() {
       res.send(result)
     })
 
+    // reco delete
+    app.delete('/recodelete/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await RecoCollenction.deleteOne(query)
+      res.send(result)
+    })
 
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
